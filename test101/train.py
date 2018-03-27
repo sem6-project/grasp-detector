a1c10a809c0874f7c6d1654b901faa2fac65ce1f
Himanshu Shekhar <himanshushekharb16@gmail.com>
2018-03-27 12:47:21 +0530

    just crashed my system, perhaps something worked

--- test101/train.py
+++ test101/train.py
@@ -34,6 +34,8 @@ class DataBatcher(Sequence):
         self.datapoints = datapoints
         self.batch_size = batch_size
 
+        print('batcher created of batch_size', self.batch_size)
+
     def __len__(self):
         # return math.floor(len(self.x) / self.batch_size)
         return int(len(self.datapoints) / self.batch_size)
@@ -41,12 +43,20 @@ class DataBatcher(Sequence):
     def __getitem__(self, idx):
         # batch_x = self.x[idx * self.batch_size : (idx+1) * self.batch_size]
         # batch_y = self.y[idx * self.batch_size : (idx+1) * self.batch_size]
+        print('batch > __getitem__ for idx', idx)
         batch = self.datapoints[idx * self.batch_size : (idx+1) * self.batch_size]
         # batch_x = np.array(list(map(lambda x: x.image_path, batch)))
-        batch_x = np.array(list(map(lambda x: utils.readImage(x.image_path),
-                                    batch)))
-        batch_y = np.array(list(map(lambda y: y.rect.to_numpy(),
-                                    batch)))
+        images = list(map(lambda x: x.get_image(), batch))
+        rectangles = list(map(lambda x: x.rect.to_numpy(), batch))
+        # batch_x = np.array(list(map(lambda x: utils.readImage(x.image_path),
+        #                             batch)))
+        # batch_y = np.array(list(map(lambda y: y.rect.to_numpy(),
+        #                             batch)))
+
+        batch_x = np.array(images)
+        batch_x = (batch_x.reshape(self.batch_size, -1) - batch_x.mean()) / batch_x.std()
+        batch_y = np.array(rectangles)
+        print('getitem (debug) :', type(batch_x), len(batch_x))
         return batch_x, batch_y
 
 
@@ -66,8 +76,18 @@ if __name__ == '__main__':
     train_seq = DataBatcher(train_set, batch_size=int(len(train_set) / n_batches))
     test_seq = DataBatcher(test_set, batch_size=int(len(test_set) / n_batches))
 
-    x_shape = utils.readImage(datapoints[0].image_path).shape[-1]
-    y_shape = datapoints[0].rect.to_numpy().shape[-1]
+    # x_shape = utils.readImage(datapoints[0].image_path).shape[-1]
+    # y_shape = datapoints[0].rect.to_numpy().shape[-1]
+    print(train_seq[0][0].shape, train_seq[0][1].shape)
+    image = train_seq[0][0][0]
+    # print(type(train_seq[0]), type(train_seq[0][0]), train_seq[0][0].shape)
+    print(type(image), image.shape, type(image))
+    # print(train_seq[0], type(train_seq[0]))
+    # some_x, some_y = train_seq[0]
+    some_x_batch, some_y_batch = train_seq[0]
+    print(some_x_batch.shape, some_y_batch.shape)
+    some_x, some_y = some_x_batch[0], some_y_batch[0]
+    x_shape, y_shape = some_x.shape[-1], some_y.shape[-1]
     model = prepareModel(x_shape, y_shape)
 
     retval = model.fit_generator(train_seq,
