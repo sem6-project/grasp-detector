4c7696a3bd4c0e0a18f84e9c7fe7f71ea89bb811
Himanshu Shekhar <himanshushekharb16@gmail.com>
2018-04-14 03:09:08 +0530

    implement tool to label rectangles

---
+++ test102/label_util.py
@@ -0,0 +1,96 @@
+import os
+import sys
+import cv2
+import numpy as np
+from enum import Enum
+
+import utils
+
+
+N_INTENTS = 2
+RECT_COLOR = [
+    (255, 0, 0),
+    (66, 110, 26),
+    (0, 0, 255)
+]
+
+
+class Cartographer(object):
+    def __init__(self, datapoint :utils.DataPoint, image_name :str) -> None:
+        self.datapoint = datapoint
+        self.image_name = image_name
+        self._image = None
+        self.rectangles = []
+        self.current_rectangle = []
+
+    @property
+    def image(self):
+        '''To ensure that loading an image is not compulsory and
+           image is read only once'''
+        if self._image is None:
+            self._image = self.datapoint.get_image(gray=False)
+        return self._image
+
+    def cleanup(self) -> None:
+        del self._image
+
+    def track_events(self, event, x, y, flag, param) -> None:
+        if event != cv2.EVENT_LBUTTONUP:
+            return
+
+        self.current_rectangle.append((x, y))
+        intent = len(self.rectangles) + 1
+        print('\rIntent', intent, self.current_rectangle, end='')
+
+        cv2.circle(self.image, (x, y), 2, RECT_COLOR[intent-1], -1)
+
+        if len(self.current_rectangle) == 3:
+            x1, y1 = self.current_rectangle[0]
+            x2, y2 = self.current_rectangle[1]
+            x3, y3 = self.current_rectangle[2]
+            x4, y4 = int(x1+x3-x2), int(y1+y3-y2)
+            self.current_rectangle.append((x4, y4))
+
+            cv2.polylines(self.image, [np.array(self.current_rectangle, np.int32)], True, RECT_COLOR[intent-1], 2)
+
+            self.rectangles.append(
+                [(x1, y1), (x2, y2), (x3, y3), (x4, y4)]
+            )
+            self.current_rectangle = []
+            print('\nIntent', intent, self.rectangles[-1], '\n')
+
+        sys.stdout.flush()
+
+
+    def label_image(self) -> None:
+        cv2.namedWindow(self.image_name)
+        cv2.setMouseCallback(self.image_name, self.track_events)
+        shall_quit = 0
+        print('::', self.image_name)
+        while True:
+            cv2.imshow(self.image_name, self.image)
+            k = cv2.waitKey(1) & 0xFF
+            if k == 27:
+                break
+
+            if k == ord('q'):
+                if shall_quit == 1:
+                    sys.exit(0)
+                print('Press q again to quit')
+                shall_quit = 1
+        cv2.destroyAllWindows()
+        self.cleanup()
+
+
+def main():
+    datapoints = utils.prepare_datapoints()
+    unique_datapoints = utils.filter_unique_datapoints(datapoints)
+    cartographers = [Cartographer(dp, dp.image_name)
+                     for dp in unique_datapoints]
+
+    for c in cartographers:
+        c.label_image()
+
+
+if __name__ == '__main__':
+    main()
